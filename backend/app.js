f2d0e123305fb18c803e6c1fdb30ee0e42a1c2a1
Alina Martynenko <martynenko.av.94@mail.ru>
2023-08-29 21:05:16 +1000

    изменение ссылки

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(requestLogger);
 
